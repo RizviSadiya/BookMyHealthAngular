4d2fa5a57a4efcc62001c00bac92c9a04e7569da
RizviSadiya <63709880+RizviSadiya@users.noreply.github.com>
2022-04-13 22:43:40 +0530

    api-home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private api:ApiService) { }
+  // reactiveForm:FormGrop
   ngOnInit(): void {
+    const body = new URLSearchParams();
+    body.set('grant_type', 'password');
+    body.set('client_id', '05e7bbe7-19ce-4175-b2cc-4b407bc825f9');
+    body.set('client_secret', '975a20ef-c0c8-424e-abff-5fe9c167e955');
+    body.set('username', 'sandboxuser');
+    body.set('password', 'vn5ZkUsYed7CM6g');
+    console.log("body", body.toString());
+    
+    // this.reativeForm = this.fb.group({
+      // grant_type:["password"],
+      // client_id:["05e7bbe7-19ce-4175-b2cc-4b407bc825f9"],
+      // client_secret:["975a20ef-c0c8-424e-abff-5fe9c167e955"],
+      // username:["zapieruser"],
+      // password:["WhiteCoats@2022"]
+//       UserName: sandboxuser
+// password: vn5ZkUsYed7CM6g
+// client-id: 05e7bbe7-19ce-4175-b2cc-4b407bc825f9
+// client_secret: 975a20ef-c0c8-424e-abff-5fe9c167e955
+    // })
+    // const body={
+    //   grant_type:"password",
+    //   client_id:"05e7bbe7-19ce-4175-b2cc-4b407bc825f9",
+    //   client_secret:"975a20ef-c0c8-424e-abff-5fe9c167e955",
+    //   username:"sandboxuser",
+    //   password:"vn5ZkUsYed7CM6g"
+    // }
+    this.api.postHeader(body.toString()).subscribe(res=>{
+      if(res){
+        console.log("res",res);
+        
+      }
+    })
   }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,68 +1,122 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-import { HttpClient,HttpHeaders,HttpClientModule } from "@angular/common/http";
-import { map} from "rxjs/operators";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpClientModule,
+
+} from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   rootUrl;
- doctor_array:any=[]
- hospital_array:any=[]
- treatment_detail_obj:any={}
+  doctor_array: any = [];
+  hospital_array: any = [];
+  treatment_detail_obj: any = {};
   doctor_list = new BehaviorSubject(this.doctor_array);
   hospital_list = new BehaviorSubject(this.hospital_array);
   treatment_detail = new BehaviorSubject(this.treatment_detail_obj);
   constructor(private http: HttpClient) {
-  this.rootUrl = 'https://7eyeitsolutions.com/medical/medical_api/index.php/Front_end_api/';
-}
+    this.rootUrl =
+      'https://auth.whitecoats.com/auth/realms/whitecoats/protocol/openid-connect/token';
+  }
 
-changeapi(x){
-  this.rootUrl = x;
-}
-set_doctor_list(data){
-let obj={
-"cat_id":data.cat_id,
-"cat_name":data.cat_name,
+  changeapi(x) {
+    this.rootUrl = x;
+  }
 
-}
-this.postapi("doctor_list_category_wise_basic_detail",obj).subscribe((res:any)=>{
-if(res.status==1){
-    this.doctor_list.next(res);
-}
-})
-}
-set_hospital_list(data){
-let obj={
-"cat_id":data.cat_id,
-"cat_name":data.cat_name,
+  loginData: any
+  setLogindata(x) {
+    sessionStorage.setItem('loginData', x)
+    this.loginData = x
+  }
 
-}
-this.postapi("hospital_list_category_wise_basic_detail",obj).subscribe((res:any)=>{
-if(res.status==1){
-    this.hospital_list.next(res);
-}
-})
-}
-set_treatement_detail(t_id){
-let obj={
-"t_id":t_id,
+  getLogindata() {
+    let loginData = sessionStorage.getItem('loginData')
+    return loginData
 
-}
-this.postapi("get_treatment_detail",obj).subscribe((res:any)=>{
-if(res.status==1){
-    this.treatment_detail.next(res);
-}
-})
-}
-postapi(x, object): any {
-    return  this.http.post(this.rootUrl + x, object).pipe(map(res => res));
   }
 
 
-  getapi(x): Observable<any>{
-    return this.http.get<any>(this.rootUrl + x).pipe(map(res => res));
+  headersDef() {
+    let header = new HttpHeaders({
+      'Content-Type': "application/x-www-form-urlencoded",
+      // "Authorization": "Bearer " + this.getLogindata()
+    });
+    // To be edited
+    const requestOptions = { headers: header }
+    return requestOptions;
+  }
+
+  postHeader(object): any {
+    return this.http.post(this.rootUrl , object, this.headersDef())
+  }
+
+  getHeader(x): any {
+    return this.http.get(this.rootUrl + x, this.headersDef())
+  }
+
+
+  // ObtendoToken(): Observable<string> {
+  //   const headers = new Headers();
+  //   headers.append('Content-Type', 'application/x-www-form-urlencoded')
+
+  //   const body = new URLSearchParams();
+  //   body.set('grant_type', 'password');
+  //   body.set('client_id', '05e7bbe7-19ce-4175-b2cc-4b407bc825f9');
+  //   body.set('client_secret', '975a20ef-c0c8-424e-abff-5fe9c167e955');
+  //   body.set('username', 'zapieruser');
+  //   body.set('password', 'WhiteCoats@2022');
+
+  //   // return this.http.post('${ApiDeSegurança}', body, new RequestOptions({headers: headers})).map(response => response.json());
+  //   return this.http.post('https://auth.whitecoats.com/auth/realms/whitecoats/protocol/openid-connect/token', body).pipe(map((res) => res));
+  // }
+
+  // set_doctor_list(data) {
+  //   let obj = {
+  //     cat_id: data.cat_id,
+  //     cat_name: data.cat_name,
+  //   };
+  //   this.postapi('doctor_list_category_wise_basic_detail', obj).subscribe(
+  //     (res: any) => {
+  //       if (res.status == 1) {
+  //         this.doctor_list.next(res);
+  //       }
+  //     }
+  //   );
+  // }
+  // set_hospital_list(data) {
+  //   let obj = {
+  //     cat_id: data.cat_id,
+  //     cat_name: data.cat_name,
+  //   };
+  //   this.postapi('hospital_list_category_wise_basic_detail', obj).subscribe(
+  //     (res: any) => {
+  //       if (res.status == 1) {
+  //         this.hospital_list.next(res);
+  //       }
+  //     }
+  //   );
+  // }
+  // set_treatement_detail(t_id) {
+  //   let obj = {
+  //     t_id: t_id,
+  //   };
+  //   this.postapi('get_treatment_detail', obj).subscribe((res: any) => {
+  //     if (res.status == 1) {
+  //       this.treatment_detail.next(res);
+  //     }
+  //   });
+  // }
+  postapi(body): any {
+    return this.http.post('https://auth.whitecoats.com/auth/realms/whitecoats/protocol/openid-connect/token', body).pipe(map((res) => res));
+  }
+
+  getapi(x): Observable<any> {
+    return this.http.get<any>(this.rootUrl + x).pipe(map((res) => res));
   }
 }
